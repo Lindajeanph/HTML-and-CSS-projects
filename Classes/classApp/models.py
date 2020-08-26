51cd5a5489cac4cfc9a603e344023cd87453379f
Linda J <jeanphlippe.linda@gmail.com>
2020-08-26 19:39:11 -0400

    added folders

--- Classes/classApp/models.py
+++ Classes/classApp/models.py
@@ -1,25 +1,23 @@
 from django.db import models
-from django.db.models.fields
+
 # Create your models here.
 
 class djangoClasses(models.Model):
     title = models.CharField(max_length=60)
     course_number = models.IntegerField
     instructor_name = models.CharField(max_length=60)
-    duration = models.FloatField(max_length= 60)
+    duration = models.FloatField(max_length=60)
 
     objects = models.Manager()
 
 class graphicDesign(models.Model):
     description = models.CharField(max_length=100)
-    schedule = models.CharField(max=60)
+    schedule = models.CharField(max_length=60)
 
 class cyberSecurity(models.Model):
     description = models.CharField(max_length=100)
-    schedule = models.CharField(max=60)
+    schedule = models.CharField(max_length=60)
 
 class helpDesk(models.Model):
     description = models.CharField(max_length=100)
     schedule = models.CharField(max_length=60)
-
-
