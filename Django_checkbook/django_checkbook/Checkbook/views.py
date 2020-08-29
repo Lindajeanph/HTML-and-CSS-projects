ebc33e098e99795c4ebeead11e195b50443c5a0d
Linda J <jeanphlippe.linda@gmail.com>
2020-08-29 12:48:35 -0400

    Checkbook

---
+++ Django_checkbook/django_checkbook/Checkbook/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'checkbook/index.html')
+
+def create_account(request):
+    return render(request, 'checkbook/CreateNewAccount.html')
+
+def balance(request):
+    return render(request, 'checkbook/BalaceSheet.html')
+
+def transaction(request):
+    return render(request, 'checkbook/AddTransaction.html')
