fd0a3987bc4d81f99e263b29fb7cec5666ce7ae9
Linda J <jeanphlippe.linda@gmail.com>
2020-09-03 18:15:40 -0400

    Another update

--- assignment5.py
+++ assignment5.py
@@ -5,7 +5,6 @@ import sys
 import datetime
 import glob
 from datetime import timedelta
-import tkinter
 destination = 'C:/Users/bposs/Documents/MonitoredFiles/'
 source = 'C:/Users/bposs/Documents/DailyFiles/'
 
@@ -21,18 +20,31 @@ for i in a:
         shutil.move(b, destination)
         print(i)
 
+from tkinter import *
+win = Tk()
+b1 = Button(win, text="Check files")
+b2 = Button(win, text="Copy Files")
+b3 = Button(win, text="Initiate")
+b1.pack()
+b2.pack()
+b3.pack()
+b1.pack(side=LEFT, padx = 20)
+b2.pack(side=LEFT, padx = 20)
+b3.pack(side=LEFT, padx = 20)
+
        #check files
 def but1():
     print(a)
-
+b1.configure(command=but1)
     #Copy files
 def but2():
     print(destination)
+b2.configure(command=but2)
 
     #Initiate
 def but3():
     print(i)
-
+b3.configure(command=but3)
 
     
 
