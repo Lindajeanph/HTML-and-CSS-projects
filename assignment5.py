2ef91bb1504a8e769ae32dfc2f0d4d04839bce18
Linda J <jeanphlippe.linda@gmail.com>
2020-09-03 17:15:49 -0400

    Updated

--- assignment5.py
+++ assignment5.py
@@ -5,10 +5,11 @@ import sys
 import datetime
 import glob
 from datetime import timedelta
+import tkinter
 destination = 'C:/Users/bposs/Documents/MonitoredFiles/'
 source = 'C:/Users/bposs/Documents/DailyFiles/'
 
-
+    #a list of files
 a = os.listdir(source)
 
 for i in a:
@@ -19,10 +20,18 @@ for i in a:
     if e >= c:
         shutil.move(b, destination)
         print(i)
-        
 
+       #check files
+def but1():
+    print(a)
 
+    #Copy files
+def but2():
+    print(destination)
 
+    #Initiate
+def but3():
+    print(i)
 
 
     
