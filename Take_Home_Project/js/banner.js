a2a2b9b31740b496bda752f0163d3beaa48e40f0
Linda J <jeanphilippe.linda@gmail.com>
2023-06-08 22:04:26 -0400

    Advertising banner

---
+++ Take_Home_Project/js/banner.js
@@ -0,0 +1,36 @@
+// {
+//     "isOfferActive": true
+//   }
+
+// function getOfferPrice(isOfferActive) {
+//     if (isOfferActive === true) {
+//       return '$139';
+//     } else {
+//       return '$119';
+//     }
+//   }
+
+// function subscribe(event) {
+//     event.preventDefault();
+//     const element = event.target;
+//     element.innerHTML = 'Thank you for subscribing!';
+//     console.log('Complete');
+//   }
+  
+
+// underlinedText.addEventListener('click', () => {
+//     element.innerHTML =  'Thank you for subscribing!'
+// })};
+  
+
+// const overlay = document.querySelector('.overlay');
+
+// document.querySelector('.image-container').addEventListener('mouseenter', () => {
+//   overlay.classList.add('active');
+// });
+
+
+
+
+
+
